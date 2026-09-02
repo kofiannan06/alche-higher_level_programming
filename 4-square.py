9d2ac46b1fc3feb59d205237a19e67263dc4e6ce
Ebenezer Kofi Annan <e.annan@alustudent.com>
2026-09-02 06:08:31 -0700

    Add Square size property

---
+++ 4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the square size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set and validate the square size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the square area."""
+        return self.__size ** 2
