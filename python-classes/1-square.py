494bc0a487c7ba094bfd2498a983f743a4ea0520
Ebenezer Kofi Annan <e.annan@alustudent.com>
2026-09-11 15:14:02 -0700

    Add Square with private size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a square with a private size."""
+        self.__size = size
